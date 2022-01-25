f08b45999f428f2924f488386fc8e6031b02e4f2
SushantTAN <punk1susant@gmail.com>
2022-01-25 22:19:37 +0545

    initialized and navbar

---
+++ src/pages/form/form.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div>
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Form Here
+      </div>
+    </div>
+  );
+}
+
+export default Form;
---
+++ src/pages/home/home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Home Here
+      </div>
+    </div>
+  );
+}
+
+export default Home;
